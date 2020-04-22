f383ddf1641d66753482265ef92fc8bf1d762226
Sem Postma <sem.postma@gmail.com>
2020-04-22 21:59:43 +0200

    [FIX] Fixed renaming

--- src/MuiForm/MuiForm.tsx
+++ src/MuiForm/MuiForm.tsx
@@ -1,6 +1,6 @@
-import { withTheme } from "react-jsonschema-form";
+import { withTheme } from 'afira-react-jsonschema-form';
 
-import Theme from "../Theme";
+import Theme from '../Theme';
 
 const MuiForm = withTheme(Theme);
 
